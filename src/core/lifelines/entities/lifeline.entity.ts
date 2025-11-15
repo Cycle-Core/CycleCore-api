c384811ff360c058fa17e9d5c01ba10e4b619c5a
jv-ransika <jvgransika@gmail.com>
2025-11-15 14:32:46 +0530

    feat: Add Procfile for production start

--- src/core/lifelines/entities/lifeline.entity.ts
+++ src/core/lifelines/entities/lifeline.entity.ts
@@ -1,6 +1,7 @@
+import { ObjectType } from "src/core/object-types/entities/object-type.entity";
 import { Episode } from "../../episodes/entities/episode.entity";
 import { ObjectEntity } from "../../objects/entities/object.entity";
-import { Column, CreateDateColumn, Entity, OneToMany, OneToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { Column, CreateDateColumn, Entity, ManyToOne, OneToMany, OneToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 
 @Entity()
 export class Lifeline {
@@ -10,11 +11,15 @@ export class Lifeline {
     @Column()   
     name: string;
 
-    @OneToOne(() => ObjectEntity, (object) => object.lifeline, { nullable: false })
+    @OneToOne(() => ObjectEntity, (object) => object.lifeline, { nullable: true })
     object: ObjectEntity;
+
+    @ManyToOne(() => ObjectType, (objectType) => objectType.lifelines, { nullable: true })
+    objectType: ObjectType;
 
     @OneToMany(() => Episode, (episode) => episode.lifeline)
     episodes: Episode[];
+    
 
     @CreateDateColumn()
     createdAt: Date;
--- src/core/object-types/entities/object-type.entity.ts
+++ src/core/object-types/entities/object-type.entity.ts
@@ -1,3 +1,4 @@
+import { Lifeline } from "src/core/lifelines/entities/lifeline.entity";
 import { ObjectEntity } from "../../objects/entities/object.entity";
 import { Column, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 
@@ -11,6 +12,9 @@ export class ObjectType {
 
     @OneToMany(() => ObjectEntity, (object) => object.objectType)
     objects: ObjectEntity[];
+
+    @OneToMany(() => Lifeline, (lifeline) => lifeline.objectType)
+    lifelines: Lifeline[];
 
     @CreateDateColumn()
     createdAt: Date;
